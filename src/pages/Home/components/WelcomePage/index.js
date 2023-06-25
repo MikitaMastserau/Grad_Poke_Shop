0f488f9c44b670c153118544e2db27e0cb1a4d83
MikitaMastserau <nik.redko@gmail.com>
2023-06-25 20:59:06 +0700

    fix backgroung and welcome page

--- src/pages/Home/components/WelcomePage/index.js
+++ src/pages/Home/components/WelcomePage/index.js
@@ -1,4 +1,4 @@
-import { SubTitle } from "components/SubTitle";
+import { Title } from "components/Title";
 
 import signInIcon from "static/icons/signInIcon.png";
 import signUpIcon from "static/icons/signUpIcon.png";
@@ -9,14 +9,15 @@ export const WelcomePage = () => {
    return (
       <>
          <div className={styles.wrapper}>
-            <SubTitle color="orangered" title="Welcome to Pokemon Online Store" />
+            <Title color="darkorange" title="Welcome to Pokemon Online Store" />
+            <hr className={styles.line} />
             <div className={styles.sign}>
-               <img src={signInIcon} alt="" />
-               <p>Log In your account and run to buy tons of Pokemon!</p>
+               {/* <img src={signInIcon} alt="" /> */}
+               <p><a href="sign-in">Log In</a> your account and run to buy tons of Pokemon!</p>
             </div>
             <div className={styles.sign}>
-               <img src={signUpIcon} alt="" />
-               <p>You still don't have an account? Quickly go Sign Up!</p>
+               {/* <img src={signUpIcon} alt="" /> */}
+               <p>You still don't have an account?! Quickly go <a href="sign-up">Sign Up</a>!!</p>
             </div>
          </div>
       </>
